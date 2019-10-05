a45d48ed9cdc866aa4eaf375f375caecfbed4f7d
mcm0399 <mcm0399@my.unt.edu>
2019-10-05 14:35:07 -0500

    camera now follows player.

---
+++ FrostyBois/Assets/FollowPlayers.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayers : MonoBehaviour
+{
+
+    public GameObject follower;
+
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - 0.5f*follower.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = follower.transform.position + offset;
+    }
+}
