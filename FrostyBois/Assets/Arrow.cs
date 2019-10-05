a9c91cfb589d8bcb5eb43dbfac9cbf72b429fce0
Jax Saunders <jax10jax@gmail.com>
2019-10-04 21:36:15 -0500

    first commit

---
+++ FrostyBois/Assets/Arrow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		point();
+     }
+
+	void point() {
+		Vector3 mousePosition = Input.mousePosition;
+		print(mousePosition.x);
+		mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+		Vector2 direction= new Vector2(
+			mousePosition.x - transform.position.x,
+			mousePosition.y - transform.position.y
+		);
+
+		transform.up = direction;
+	}
+}
---
+++ FrostyBois/Assets/Player.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour {
+
+    public float thrust;
+	public bool up;
+	[SerializeField] public float maxSpeed;
+	[SerializeField] public Rigidbody2D body;
+	[SerializeField] public float increment;
+	// Use this for initialization
+	void Start () {
+		up = true;	
+		body = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.Space))
+        {
+			if (up)
+			{
+				thrust+= increment;
+				if (thrust >= maxSpeed)
+				{
+					up = false;
+				}
+			}
+			else
+			{
+				thrust-= increment;
+				if (thrust <= 0.0f)
+				{
+					up = true;
+				}
+			}		
+        }
+		if (Input.GetKeyUp(KeyCode.Space))
+		{
+			thrust += 100.0f;
+			print("Final thrust " + thrust);
+			body.AddForce(new Vector3(1, 1, 0) * thrust);
+			thrust = 0.0f;
+		}
+	}
+}
